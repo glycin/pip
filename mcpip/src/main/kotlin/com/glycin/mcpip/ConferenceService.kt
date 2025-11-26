e666a52752b79c2de4c023cbed6ea7e96540605e
jdrAlexander <alex.chatzizacharias@gmail.com>
2025-11-26 20:41:33 +0100

    added kotlin dev day to conferences

--- mcpip/src/main/kotlin/com/glycin/mcpip/ConferenceService.kt
+++ mcpip/src/main/kotlin/com/glycin/mcpip/ConferenceService.kt
@@ -13,6 +13,7 @@ class ConferenceService {
         Conference(LocalDate.of(2025, 9, 23), LocalDate.of(2025, 10, 1), "Test", "This be a test conference yarr!"),
         Conference(LocalDate.of(2025, 10, 6), LocalDate.of(2025, 10, 10), "Devoxx", "The one that started it all in Antwerp Belgium, founded by Stephan Janssen"),
         Conference(LocalDate.of(2025, 11, 5), LocalDate.of(2025, 11, 7), "JFall", "Organized by the NLJUG in Ede, the Netherlands. Say hi to your colleagues!"),
+        Conference(LocalDate.of(2025, 11, 26), LocalDate.of(2025, 11, 28), "Kotlin Dev Day", "Organized by the Xebia in Amsterdam, the Netherlands. Say hi to all Kotlin fanboys!!!"),
         Conference(LocalDate.of(2026, 2, 2), LocalDate.of(2026, 2, 4), "JFokus", "Amazing conference in Stockholm. This years theme is: Norse gods!")
     )
 
