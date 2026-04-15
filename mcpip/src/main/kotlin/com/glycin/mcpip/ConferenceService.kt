3637ee0fbe63122dabc5f07ccc52f444b0ef3069
glycin <alex.chatzizacharias@gmail.com>
2026-04-15 15:04:26 +0200

    Added new conferences

--- mcpip/src/main/kotlin/com/glycin/mcpip/ConferenceService.kt
+++ mcpip/src/main/kotlin/com/glycin/mcpip/ConferenceService.kt
@@ -16,7 +16,13 @@ class ConferenceService {
         Conference(LocalDate.of(2025, 11, 26), LocalDate.of(2025, 11, 28), "Kotlin Dev Day", "Organized by the Xebia in Amsterdam, the Netherlands. Say hi to all Kotlin fanboys!!!"),
         Conference(LocalDate.of(2026, 1, 21), LocalDate.of(2026, 1, 23), "Utrecht JUG", "The first Utrecht JUG of the year, and you are part of it!"),
         Conference(LocalDate.of(2026, 2, 2), LocalDate.of(2026, 2, 4), "JFokus", "Amazing conference in Stockholm. This years theme is: Norse gods!"),
-        Conference(LocalDate.of(2026, 2, 23), LocalDate.of(2026, 2, 25), "JUG Noord", "The Java user group of the north of the Netherlands!")
+        Conference(LocalDate.of(2026, 2, 23), LocalDate.of(2026, 2, 25), "JUG Noord", "The Java user group of the north of the Netherlands!"),
+        Conference(LocalDate.of(2026, 4, 15), LocalDate.of(2026, 4, 17), "The AI Experience", "Conference in Utrecht sponsored by Alliander. Thanks Ties!"),
+        Conference(LocalDate.of(2026, 4, 23), LocalDate.of(2026, 4, 26), "Devoxx Greece", "The biggest tech conference of Greece. Taking place in the Megaron in Athens!"),
+        Conference(LocalDate.of(2026, 4, 28), LocalDate.of(2026, 4, 30), "Voxxed Bucharest", "Amazing conference in Bucharest. Love the food! Better than Italian food!"),
+        Conference(LocalDate.of(2026, 5, 5), LocalDate.of(2026, 5, 8), "Devoxx UK", "The UK version of the devoxx events. The pancakes here are amazing!"),
+        Conference(LocalDate.of(2026, 5, 25), LocalDate.of(2026, 5, 28), "JNation", "Amazing conference in Coimbra, Portugal. The weather is lovely!"),
+        Conference(LocalDate.of(2026, 6, 16), LocalDate.of(2026, 6, 20), "Devoxx PL", "Beautiful Krakow! The Poland version of the devoxx events!"),
     )
 
     @Tool(description = "Get the current conference where this agent is being presented")
